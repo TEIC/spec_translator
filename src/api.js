309c6ebd0fb6de6c06d56340807ff40c864baa1a
Hugh Cayless <philomousos@gmail.com>
2022-06-16 16:41:14 -0400

    Don't create extra folder.

--- src/api.js
+++ src/api.js
@@ -310,6 +310,7 @@ export async function commit(file, content, message) {
   // get the tree for the new blob's path -> old_blob_tree_sha
   let path = (file.match(/\//) ? file : 'P5/Source/Specs/' + file).replace(/\/[^\/]+$/,'').split('/');
   let tree = await buildTree(trees, path.join('/'), file.replace(/.+\/([^\/]+)$/, "$1"), blob.sha, true);
+  path.pop();
   while (path.length > 1) {
     tree = await buildTree(trees, path.join('/'), path.pop(), tree.sha, false);
   }
